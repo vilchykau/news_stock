e432a37cb3e0a80bc2bcd5cab8a77edcd6c886bb
Stanislau Vilchykau <donsgenii@gmail.com>
2022-04-02 19:38:30 +0300

    feat: add publish date to detaled layer

--- database/routines/DWH_LOAD_NEWS.sql
+++ database/routines/DWH_LOAD_NEWS.sql
@@ -1,11 +1,23 @@
-create or replace procedure DWH.LOAD_NEWS()
-    language PLPGSQL
+create or replace procedure DWH.load_news()
+    language plpgsql
 as
 $$
 declare
 begin
-    insert into DWH.LNE_NEWS(NE_TIT_NEWS_TITLE, NE_DES_NEWS_DESCRIPTION, METADATA_NE)
-    select TITLE, DESCRIPTION, 1
-    from SA.NEWS_COMPANY;
+    insert into DWH.LNE_NEWS( NE_TIT_NEWS_TITLE
+                            , NE_DES_NEWS_DESCRIPTION
+                            , ne_pdt_news_publishdt
+                            , METADATA_NE)
+    select distinct
+           TITLE
+         , DESCRIPTION
+         , publish_date
+         , 1
+    from SA.NEWS_COMPANY
+             left join DWH.LNE_NEWS dwh
+                       on title = dwh.ne_tit_news_title
+                           and dwh.ne_des_news_description = description
+                           and publish_date = ne_pdt_news_publishdt
+    where dwh.ne_tit_news_title is null;
 END;
 $$;
