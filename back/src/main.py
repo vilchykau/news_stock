7ba7c70552e18baa8a49b70e55ada67fe9f9eee1
Stanislau Vilchykau <donsgenii@gmail.com>
2022-05-15 16:37:45 +0300

    feat: add api for chart page

--- back/src/main.py
+++ back/src/main.py
@@ -23,17 +23,69 @@ app.add_middleware(
 )
 
 
-
 @app.get("/")
 def read_root():
     return {'news': [{'title': 'some title 1', 'company': 'Nviia'}]}
 
+
 @app.get("/api/news_list")
-def read_root():
+def read_news():
     conn = psycopg2.connect(dbname='airflow', user='airflow',
                             password='airflow', host='localhost')
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM user_space.vw_company_news limit 20')
+    records = cursor.fetchall()
+    cursor.close()
+    conn.close()
+
+    news_list = []
+    for n in records:
+        description = n[1]
+        if description is not None and len(description) > 120:
+            description = description[0:115] + '...'
+
+        news_list.append({
+            'title': n[0],
+            'description': description,
+            'company': n[2],
+            'publish_timestamp': n[3],
+            'image_url': n[4]
+        })
+
+    return {'news': news_list}
+
+
+@app.get('/api/trade')
+def read_stocks():
+    conn = psycopg2.connect(dbname='airflow', user='airflow',
+                            password='airflow', host='localhost')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM user_space.vw_stocks')
+    records = cursor.fetchall()
+    cursor.close()
+    conn.close()
+
+    news_list = []
+    for n in records:
+        news_list.append({
+            'open': n[0],
+            'close': n[1],
+            'high': n[2],
+            'low': n[3],
+            'date': n[5]
+        })
+    return {'trade': news_list}
+
+
+@app.get("/api/news_list_date/{date_str}")
+def news_list_date(date_str: str):
+    conn = psycopg2.connect(dbname='airflow', user='airflow',
+                            password='airflow', host='localhost')
+    cursor = conn.cursor()
+    if date_str != 'null':
+        cursor.execute(f'SELECT * FROM user_space.vw_company_news where "date" = date \'{date_str}\' ')
+    else:
+        cursor.execute('SELECT * FROM user_space.vw_company_news limit 20')
     records = cursor.fetchall()
     cursor.close()
     conn.close()
